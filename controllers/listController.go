a9aadcfcfa69f30642ad131b20d788dc95f79140
Idhibhat Pankam <marginpankam@gmail.com>
2023-05-23 15:12:23 +0700

    feat: basic crud for list

--- controllers/listController.go
+++ controllers/listController.go
@@ -1,1 +1,67 @@
 package controllers
+
+import (
+	"github.com/bookpanda/champ-eng-go-crud/database"
+	"github.com/bookpanda/champ-eng-go-crud/models"
+	"github.com/gin-gonic/gin"
+)
+
+func GetLists(c *gin.Context) {
+	var lists []models.List
+	database.DB.Find(&lists)
+
+	c.JSON(200, gin.H{
+		"lists": lists,
+	})
+}
+
+func GetList(c *gin.Context) {
+	id := c.Param("id")
+	var list models.List
+	database.DB.First(&list, id)
+
+	c.JSON(200, gin.H{
+		"list": list,
+	})
+}
+
+func CreateList(c *gin.Context) {
+	var body struct {
+		Title string
+		Order int
+	}
+	c.Bind(&body)
+	list := models.List{Title: body.Title, Order: body.Order}
+	database.DB.Create(&list)
+
+	c.JSON(200, gin.H{
+		"list": list,
+	})
+}
+
+func UpdateList(c *gin.Context) {
+	id := c.Param("id")
+	var body struct {
+		Title string
+		Order int
+	}
+	c.Bind(&body)
+	var list models.List
+	database.DB.First(&list, id)
+	database.DB.Model(&list).Updates(models.List{Title: body.Title, Order: body.Order})
+
+	c.JSON(200, gin.H{
+		"list": list,
+	})
+}
+
+func DeleteList(c *gin.Context) {
+	id := c.Param("id")
+	var list models.List
+	database.DB.First(&list, id)
+	database.DB.Delete(&models.List{}, id)
+
+	c.JSON(200, gin.H{
+		"list": list,
+	})
+}
--- routes/listRoutes.go
+++ routes/listRoutes.go
@@ -1,7 +1,14 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/bookpanda/champ-eng-go-crud/controllers"
+	"github.com/gin-gonic/gin"
+)
 
 func SetUpListRoutes(r *gin.Engine) {
-	r.GET("/lists")
+	r.GET("/lists", controllers.GetLists)
+	r.GET("/lists/:id", controllers.GetList)
+	r.POST("/lists", controllers.CreateList)
+	r.PUT("/lists/:id", controllers.UpdateList)
+	r.DELETE("/lists/:id", controllers.DeleteList)
 }
